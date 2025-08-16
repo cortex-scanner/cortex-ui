9795b03cfd91adb2721e2cad836e1d9841748581
Steffen Schön <steffen1995@gmail.com>
2025-08-16 21:36:15 +0200

    feat: configure linting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,13 @@
 // @ts-check
-import withNuxt from './.nuxt/eslint.config.mjs'
+import withNuxt from "./.nuxt/eslint.config.mjs";
+import stylistic from "@stylistic/eslint-plugin";
 
 export default withNuxt(
-  // Your custom configs here
-)
+  stylistic.configs.customize({
+    commaDangle: "never",
+    quotes: "double",
+    semi: true,
+    indent: 2,
+    braceStyle: "1tbs"
+  })
+);
