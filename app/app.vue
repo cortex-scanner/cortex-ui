2a222cf43cc5e610f985bc13aece4ece18a46c80
Steffen Schön <steffen1995@gmail.com>
2025-08-16 22:06:50 +0200

    feat: add nuxt-ui

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,8 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <UApp>
+    <div>
+      <UButton>Click me</UButton>
+      <span class="text-xl">Hello World</span>
+    </div>
+  </UApp>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,16 @@
+import tailwindcss from "@tailwindcss/vite";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-
+  css: ["~/assets/css/main.css"],
   modules: [
     "@nuxt/eslint",
     "@nuxt/icon",
     "@nuxt/fonts",
-    "@nuxt/test-utils"
-  ]
+    "@nuxt/test-utils",
+    "@nuxt/ui"
+  ],
+  vite: { plugins: [tailwindcss()] }
 });
