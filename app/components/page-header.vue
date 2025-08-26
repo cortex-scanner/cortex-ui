6405eab3fc26c1c8cb1e84a72f664ebaa9300c80
Steffen Schön <steffen1995@gmail.com>
2025-08-26 20:40:28 +0200

    feat: add support for disabling page header actions

--- app/components/page-header.vue
+++ app/components/page-header.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 
 export interface PageAction {
+  id?: string;
   label: string;
   icon: string;
   color?: "primary" | "secondary" | "success" | "info" | "warning" | "error" | "neutral";
+  disabled?: boolean;
   action: () => void | Promise<void>;
 }
 
@@ -22,12 +24,12 @@ const { title, actionsStart = [], actionsEnd = [] } = defineProps<Props>();
   <span class="text-2xl mb-3">{{ title }}</span>
   <div class="flex items-center justify-between">
     <div v-if="actionsStart" class="flex gap-1">
-      <UButton v-for="action in actionsStart" :key="action.label" :icon="action.icon" :color="action.color || 'neutral'" variant="ghost" class="py-2" @click="action.action">
+      <UButton v-for="action in actionsStart" :key="action.id || action.label" :icon="action.icon" :color="action.color || 'neutral'" :disabled="action.disabled" variant="ghost" class="py-2" @click="action.action">
         {{ action.label }}
       </UButton>
     </div>
     <div v-if="actionsEnd" class="flex gap-2">
-      <UButton v-for="action in actionsEnd" :key="action.label" :icon="action.icon" :color="action.color || 'neutral'" variant="ghost" class="py-2" @click="action.action">
+      <UButton v-for="action in actionsEnd" :key="action.id || action.label" :icon="action.icon" :color="action.color || 'neutral'" :disabled="action.disabled" variant="ghost" class="py-2" @click="action.action">
         {{ action.label }}
       </UButton>
     </div>
